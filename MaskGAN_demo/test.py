d15977a65ef9e6344ee40dfe1a4ae654ea65bc94
ashish-roopan <ashishroopan000@gmail.com>
2020-04-12 07:19:09 +0530

    changed dir names and added mapping.txt

---
+++ MaskGAN_demo/test.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import cv2
+import time
+import numpy as np
+from PIL import Image
+
+import torch
+from torchvision.utils import save_image
+
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+from data.base_dataset import BaseDataset, get_params, get_transform, normalize
+
+from ui.ui import Ui_Form
+from ui.mouse_event import GraphicsScene
+from ui_util.config import Config
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
+
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = str(0)
+#model = Model(config)
+opt = TestOptions().parse(save=False)
+print(opt)
+print(type(opt))
+print('opt,model',opt.model)
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+model = create_model(opt)
+
+
+mask=cv2.imread('samples/0.png')
+mask_m=cv2.imread('../../gdrive/My Drive/faceswap/mask3.png')
+mat_img = Image.open('samples/0.jpg')
+img = mat_img.copy()
+
+params = get_params(opt, (512,512))
+transform_mask = get_transform(opt, params, method=Image.NEAREST, normalize=False, normalize_mask=True)
+transform_image = get_transform(opt, params)
+
+
+
+mask = transform_mask(Image.fromarray(np.uint8(mask))) 
+mask_m = transform_mask(Image.fromarray(np.uint8(mask_m)))
+img = transform_image(img)
+
+
+
+start_t = time.time()
+generated = model.inference(torch.FloatTensor([mask_m.numpy()]), torch.FloatTensor([mask.numpy()]), torch.FloatTensor([img.numpy()]))   
+end_t = time.time()
+print('inference time : {}'.format(end_t-start_t))
+#save_image((generated.data[0] + 1) / 2,'./results/1.jpg')
+result = generated.permute(0, 2, 3, 1)
+result = result.cpu().detach().numpy()
+result = (result + 1) * 127.5
+result = np.asarray(result[0,:,:,:], dtype=np.uint8)
+result= cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+cv2.imwrite('gen.jpg',result)
+print(result)
+
+
+# # app = QApplication(sys.argv)
+# ex = Ex(model, opt)
--- face_parsing/Data_preprocessing/g_color.py
+++ face_parsing/Data_preprocessing/g_color.py
@@ -5,7 +5,7 @@ import numpy as np
 from utils import make_folder
 color_list = [[0, 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51, 0], [255, 153, 51], [0, 204, 0]]
 
-folder_base = 'CelebAMask-HQ-mask'
+folder_base = 'CelebAMaskHQ-mask'
 folder_save = 'CelebAMask-HQ-mask-color'
 img_num = 10
 
@@ -17,7 +17,7 @@ for k in range(img_num):
         im_base = np.zeros((512, 512, 3))
         im = Image.open(filename)
         im = np.array(im)
-        im = im[:, :, 0]
+        #im = im[:, :, 0]
         for idx, color in enumerate(color_list):
             im_base[im == idx] = color
     filename_save = os.path.join(folder_save, str(k) + '.png')
